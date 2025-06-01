94cb7701e3c7a79886ba990688e79ad6c6b0f107
sujnanachar@gmail.com <sujnanachar@gmail.com>
2025-06-01 16:45:37 +0530

    first commit

---
+++ src/main/java/com/program2/maven/program2/App.java
@@ -0,0 +1,13 @@
+package com.program2.maven.program2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
